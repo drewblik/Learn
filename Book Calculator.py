9601b33c845a1f4e8c8f1be098c982ca43701c47
drewblik <drewleeblik@gmail.com>
2019-08-09 21:13:24 -0700

    book calculator

---
+++ Book Calculator.py
@@ -0,0 +1,8 @@
+import math
+
+#typecasting is converting a string to an int
+pages = int(input("Enter number of pages:"))
+words = int(input("Enter number of words on one page: "))
+hh_finish = round((pages*words/270/60)*2)
+print(f"You will finish this book in {hh_finish} days!")
+
