831f7daf202db37b291efbff2ddc8260d462ec89
Drew Blik <drewblik@comcast.net>
2019-08-11 15:12:03 -0700

    added book storing feature

--- Book Calculator.py
+++ Book Calculator.py
@@ -1,9 +1,14 @@
 import math
 
 #typecasting is converting a string to an int
+name = input("Enter book name: ")
 pages = int(input("Enter number of pages:"))
 words = int(input("Enter number of words on one page: "))
 hh_finish = round((pages*words/270/60)*2)
-print(f"You will finish this book in {hh_finish} days!")
-#add excel function (or google sheets function?)
+
+print("You will finish this book in "  + str(hh_finish)+  " days!")
+file1 = open(r"C:\Users\drewb\Desktop/BookList.txt","a")
+file1.write(name)
+file1.write( "\n"+str(hh_finish) + " days to finish")
+
 
