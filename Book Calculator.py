c03e28329804ed0a2657a9504d54dec9c5fda708
drewblik <drewleeblik@gmail.com>
2019-08-11 14:48:31 -0700

    book calculator idea

--- Book Calculator.py
+++ Book Calculator.py
@@ -5,4 +5,5 @@ pages = int(input("Enter number of pages:"))
 words = int(input("Enter number of words on one page: "))
 hh_finish = round((pages*words/270/60)*2)
 print(f"You will finish this book in {hh_finish} days!")
+#add excel function (or google sheets function?)
 
