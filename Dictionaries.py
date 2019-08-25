cd59e1c1e3f5f5169a22ed4ab991dfddbea49649
Drew Blik <drewblik@comcast.net>
2019-08-24 17:01:23 -0700

    learned dictionaries

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,4 +1,16 @@
 d = {}
 # d = {"George": 24, "Tom": 32}
 d["George"] = 24
-d[]
+d["Tom"] = 32
+print(d["George"])
+d["Jenny"] = 20
+print(d)
+#keys are strings and numbers
+d[10] = 100
+print(d[10])
+for key, value in d.items():
+    print("Key:")
+    print(key)
+    print("Value:")
+    print(value)
+    print("")
