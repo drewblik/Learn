61472325b64220904438ddb7fd7b75f3ded98a80
Drew Blik <drewblik@comcast.net>
2019-08-31 18:38:13 -0700

    learned classes and objects again

--- Classes and Objects.py
+++ Classes and Objects.py
@@ -17,3 +17,21 @@ r1 = Robot("Tom", "red", 30)
 r2 = Robot("Jerry", "blue", 40)
 r1.introduce_self()
 r2.introduce_self()
+#ctrl + k = committ
+#ctrl + alt + k = push
+class Person:
+    def __init__(self, n, p, i):
+        self.name = n
+        self.personality = p
+        self.is_sitting = i
+
+    def sit_down(self):
+        self.is_sitting = True
+
+    def stand_up(self):
+        self.is_sitting = False
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky","talkitive", True)
+p1.robot_owned = r2
+p2.robot_owned = r1
+p1.robot_owned.introduce_self()
